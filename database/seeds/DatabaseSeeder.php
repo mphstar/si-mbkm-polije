f7c5a48d52aaf8396cb2114bcafdd9fe968e552b
andruchristo27 <e41211364@student.polije.ac.id>
2023-10-24 16:25:33 +0700

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Student;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123'),
+            'remember_token' => Str::random(10),
+        ]);
+        Student::create([
+            'name' => 'student',
+            'address' => '123 Main Street',
+            'phone' => '1234567890',
+            'nim' => '123456789',
+            'email' => 'student@gmail.com',
+            'username' => 'student',
+            'password' => Hash::make('123'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
