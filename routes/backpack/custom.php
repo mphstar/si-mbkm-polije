89b895bfd8d94eef8e8b61ff28c8f341b101249d
mphstar <bintang.simdig20@gmail.com>
2023-11-04 09:43:21 +0700

    auth all level

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -2,6 +2,8 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\DosenMiddleware;
+use App\Http\Middleware\KaprodiMiddleware;
 use App\Http\Middleware\PartnerMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -31,34 +33,34 @@ Route::group([
         Route::get('penilaian-mitra/{id}/updating', 'PenilaianMitraCrudController@updating')->name("grader_partner");
         Route::post('penilaian-mitra/{id}/penilaian ', 'PenilaianMitraCrudController@penilaian');
     });
-
-
-    Route::crud('acctive-account-mitra', 'AcctiveAccountMitraCrudController');
- 
-    Route::crud('validasi-mbkm', 'ValidasiMbkmCrudController');
-
-    Route::crud('departmen', 'DepartmenCrudController');
-    Route::crud('lecturer', 'LecturerCrudController');
-    Route::get('mbkm/{id}/reg-mbkm', 'MbkmCrudController@register');
-    Route::post('mbkm/{id}/addreg', 'MbkmCrudController@addreg');
-    Route::get('mbkm-report', 'MbkmReportCrudController@viewReport');
-    Route::post('mbkm-report-upload', 'MbkmReportCrudController@upReport');
-    Route::post('mbkm-report-rev', 'MbkmReportCrudController@revReport');
-    Route::get('validasilaporan/{id}/detail_laporan ', 'ValidasilaporanCrudController@detail_laporan')->name("detail_laporan");
-    Route::crud('status-reg', 'StatusRegCrudController');
-    Route::post('validasilaporan/{id}/approve-laporan', 'ValidasilaporanCrudController@validasilaporan');
-    Route::crud('manage-student', 'ManageStudentCrudController');
     
-    Route::get('manage-student/{id}/edit', 'ManageStudentCrudController@formEdit');
-    Route::post('manage-student/{id}/editDosen', 'ManageStudentCrudController@editDosen');
-    Route::post('manage-student/{id}/editMatkul', 'ManageStudentCrudController@editMatkul');
-
-    Route::crud('nilaimbkm', 'NilaimbkmCrudController');
-
-    Route::crud('nilaimbkm', 'NilaimbkmCrudController');
-    Route::get('/nilaimbkm/{id}/inputnilai', 'NilaimbkmCrudController@inputNilai');
-    Route::post('/nilaimbkm/{id}/prosesNilai', 'NilaimbkmCrudController@prosesNilai');
+    Route::middleware([KaprodiMiddleware::class])->group(function () {
+        Route::crud('acctive-account-mitra', 'AcctiveAccountMitraCrudController');
+        Route::crud('validasi-mbkm', 'ValidasiMbkmCrudController');
+        Route::crud('departmen', 'DepartmenCrudController');
+        Route::crud('lecturer', 'LecturerCrudController');
+        Route::crud('manage-student', 'ManageStudentCrudController');
+        Route::get('manage-student/{id}/edit', 'ManageStudentCrudController@formEdit');
+        Route::post('manage-student/{id}/editDosen', 'ManageStudentCrudController@editDosen');
+        Route::post('manage-student/{id}/editMatkul', 'ManageStudentCrudController@editMatkul');
+    });
+    
+    Route::middleware([DosenMiddleware::class])->group(function () {
+        Route::crud('nilaimbkm', 'NilaimbkmCrudController');
+        Route::crud('nilaimbkm', 'NilaimbkmCrudController');
+        Route::get('/nilaimbkm/{id}/inputnilai', 'NilaimbkmCrudController@inputNilai');
+        Route::post('/nilaimbkm/{id}/prosesNilai', 'NilaimbkmCrudController@prosesNilai');
+        Route::crud('progress-mahasiswa', 'ProgressMahasiswaCrudController');
+        Route::get('mbkm/{id}/reg-mbkm', 'MbkmCrudController@register');
+        Route::post('mbkm/{id}/addreg', 'MbkmCrudController@addreg');
+        Route::get('mbkm-report', 'MbkmReportCrudController@viewReport');
+        Route::post('mbkm-report-upload', 'MbkmReportCrudController@upReport');
+        Route::post('mbkm-report-rev', 'MbkmReportCrudController@revReport');
+        Route::get('validasilaporan/{id}/detail_laporan ', 'ValidasilaporanCrudController@detail_laporan')->name("detail_laporan");
+        Route::crud('status-reg', 'StatusRegCrudController');
+        Route::post('validasilaporan/{id}/approve-laporan', 'ValidasilaporanCrudController@validasilaporan');
+        
+    });
 
     Route::get('/download/{name}', 'DownloadController@download');
-    Route::crud('progress-mahasiswa', 'ProgressMahasiswaCrudController');
 }); // this should be the absolute last line of this file
