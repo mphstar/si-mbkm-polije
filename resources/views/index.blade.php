e92c1a008bed201d54028edd4340906d1a7a9381
Syahroni12 <ronixria@gmail.com>
2023-11-24 14:03:30 +0700

    icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,7 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" href="{{ asset('assets/img/polijeLogo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/css/normalize.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.css') }}">
